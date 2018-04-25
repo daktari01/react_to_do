cbbc5abc80cf751936df419553b47654160644d4
James Dindi <dindijjames@gmail.com>
2018-04-25 07:17:07 +0300

    Add input content as state

--- src/NoteForm/NoteForm.jsx
+++ src/NoteForm/NoteForm.jsx
@@ -4,13 +4,16 @@ import './NoteForm.css';
 class NoteForm extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            newNoteContent: '',
+        };
     }
     render(){
         return(
             <div className="formWrapper">
                 <input className="noteInput"
-                placeholder="Write a note ..."/>
+                placeholder="Write a note ..."
+                value={this.state.newNoteContent} />
                 <button className="noteButton">Add Note</button>
             </div>
         );
